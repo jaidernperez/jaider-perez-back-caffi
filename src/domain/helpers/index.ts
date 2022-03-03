48173d9817968d787ddf43379177003fe489eccd
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-03-02 20:04:02 -0500

    feat: add validations helpers

---
+++ src/domain/helpers/index.ts
@@ -0,0 +1,1 @@
+export {RequestValidation} from "./validations/RequestValidation";
---
+++ src/domain/helpers/validations/RequestValidation.ts
@@ -0,0 +1,19 @@
+import * as Joi from "joi";
+import {PersonRequest} from "../../usecases";
+
+export class RequestValidation {
+
+    public static validate(request: PersonRequest): void {
+        const {error} = Joi.valid(RequestValidation.getValidationRequestSchema(), request);
+        console.log('El error: ', error);
+    }
+
+    private static getValidationRequestSchema(): Joi.Schema {
+        return Joi.object().keys({
+            name: Joi.string().alphanum().min(3).max(50).required(),
+            documentType: Joi.string().valid('CC', 'TI', 'RC', 'CE').required(),
+            document: Joi.string().alphanum().min(4).max(13).required()
+        });
+    }
+
+}
