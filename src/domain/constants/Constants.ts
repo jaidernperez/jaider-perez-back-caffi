2137a0ce81b89be83384791e1563505f9280d4b5
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-03-02 21:46:53 -0500

    fix: improve use case

--- src/domain/constants/Constants.ts
+++ src/domain/constants/Constants.ts
@@ -2,4 +2,5 @@ export class Constants {
 
     public static DOCUMENT_ALREADY_EXISTS: string = 'El número de documento ya se encuentra registrado';
     public static ID_NOT_VALID: string = 'El id no es válido';
+    public static PERSON_NOT_FOUND: string = 'La persona no ha sido encontrada';
 }
--- src/domain/usecases/person/PersonUseCase.ts
+++ src/domain/usecases/person/PersonUseCase.ts
@@ -15,38 +15,54 @@ export class PersonUseCase {
     }
 
     public findPersonById(id: number): Promise<PersonResponse> {
-        return this.mapper.entityToResponseProm(this.repository.findById(id));
+        return this.mapper.entityToResponse(this.repository.findById(id)).catch(() => {
+            throw new Error(Constants.PERSON_NOT_FOUND);
+        });
     }
 
     public findPersonByDocument(document: string): Promise<PersonResponse> {
-        return this.mapper.entityToResponseProm(this.repository.findByDocument(document));
+        return this.mapper.entityToResponse(this.repository.findByDocument(document)).catch(() => {
+            throw new Error(Constants.PERSON_NOT_FOUND);
+        });
     }
 
     public createPerson(request: PersonRequest): Promise<PersonResponse> {
         RequestValidation.validate(request);
-        this.validatePersonDocument(request.document);
-        return this.mapper.entityToResponseProm(this.repository.save(this.mapper.requestToEntity(request)));
+        this.validatePersonDocument(request.document).then(() => {});
+        return this.mapper.entityToResponse(this.repository.save(this.mapper.requestToEntity(request)));
     }
 
     public updatePerson(request: PersonRequest): Promise<PersonResponse> {
-        this.validatePersonId(request.id);
+        this.validatePersonId(request.id).then(() => {});
         RequestValidation.validate(request);
-        this.validatePersonDocument(request.document);
-        return this.mapper.entityToResponseProm(this.repository.save(this.mapper.requestToEntity(request)));
+        this.validatePersonDocument(request.document).then(() => {});
+        return this.mapper.entityToResponse(this.repository.save(this.mapper.requestToEntity(request)));
     }
 
     public findAllPersons(): Promise<PersonResponse[]> {
         return this.mapper.listEntityToResponse(this.repository.findAll());
     }
 
-    private validatePersonDocument(document: string): void {
-        if (this.repository.existByDocument(document)) {
+    public deletePerson(id: number): void {
+        this.repository.delete(id).then(r => console.log(r));
+    }
+
+    private async validatePersonDocument(document: string): Promise<void> {
+        let exists;
+        await this.repository.existsByDocument(document).then(value => {
+            exists = value;
+        })
+        if (exists) {
             throw new Error(Constants.DOCUMENT_ALREADY_EXISTS);
         }
     }
 
-    private validatePersonId(id: number): void {
-        if (id !== null && this.repository.existsById(id)) {
+    private async validatePersonId(id: number): Promise<void> {
+        let exists;
+        await this.repository.existsById(id).then(value => {
+            exists = value;
+        })
+        if (!exists) {
             throw new Error(Constants.ID_NOT_VALID);
         }
     }
