d42d65f82fc7bee5a928265a3fbc4e2e80b8f0e9
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-03-02 20:04:19 -0500

    feat: add constants class

---
+++ src/domain/constants/Constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+
+    public static DOCUMENT_ALREADY_EXISTS: string = 'El número de documento ya se encuentra registrado';
+    public static ID_NOT_VALID: string = 'El id no es válido';
+}
