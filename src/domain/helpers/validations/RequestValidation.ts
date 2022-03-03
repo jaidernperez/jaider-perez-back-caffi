3701730ca6f90dd82b22fa121f3dc7d8234c05f6
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-03-03 10:27:17 -0500

    fix: fix validation

--- src/domain/helpers/validations/RequestValidation.ts
+++ src/domain/helpers/validations/RequestValidation.ts
@@ -1,15 +1,27 @@
 import * as Joi from "joi";
 import {PersonRequest} from "../../usecases";
+import {Constants} from "../../constants/Constants";
 
 export class RequestValidation {
 
     public static validate(request: PersonRequest): void {
-        const {error} = Joi.valid(RequestValidation.getValidationRequestSchema(), request);
-        console.log('El error: ', error);
+        const {error} = RequestValidation.getValidationRequestSchema()
+            .validate(request);
+
+        if (error != null) {
+            const {details} = error;
+            const message = details.map(i => i.message).join(',');
+            throw new Error(message.replace('"',"'"));
+        }
+    }
+
+    public static async validateAsync(request: PersonRequest): Promise<string> {
+        return await RequestValidation.getValidationRequestSchema()
+            .validateAsync(request);
     }
 
     private static getValidationRequestSchema(): Joi.Schema {
-        return Joi.object().keys({
+        return Joi.object({
             name: Joi.string().alphanum().min(3).max(50).required(),
             documentType: Joi.string().valid('CC', 'TI', 'RC', 'CE').required(),
             document: Joi.string().alphanum().min(4).max(13).required(),
