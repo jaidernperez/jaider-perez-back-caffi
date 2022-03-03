69c74b66f5153a9bec7d93f808318cfe6e27619a
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-03-02 22:23:39 -0500

    feat: add controller

--- src/infrastructure/entry-points/api-rest/controllers/PersonController.ts
+++ src/infrastructure/entry-points/api-rest/controllers/PersonController.ts
@@ -10,12 +10,76 @@ export class PersonController {
     }
 
     public routes(app: express.Application): void {
-        app.route("/home").get((req: express.Request, res: express.Response) => {
-            this.loanUseCase.home().then((response) => {
+        app.route("/persons").get((req: express.Request, res: express.Response) => {
+            this.loanUseCase.findAllPersons().then((response) => {
                 res.status(200).send({
-                    message: response
+                    response
                 });
-            })
-        })
+            }).catch(reason => {
+                res.status(400).send({
+                    "error": reason
+                });
+            });
+        });
+
+        app.route("/persons/save").post((req: express.Request, res: express.Response) => {
+            this.loanUseCase.createPerson(req.body).then((response) => {
+                res.status(200).send({
+                    response
+                });
+            }).catch(reason => {
+                res.status(400).send({
+                    "error": reason
+                });
+            });
+        });
+
+        app.route("/persons/update").post((req: express.Request, res: express.Response) => {
+            this.loanUseCase.updatePerson(req.body).then((response) => {
+                res.status(200).send({
+                    response
+                });
+            }).catch(reason => {
+                res.status(400).send({
+                    "error": reason
+                });
+            });
+        });
+
+        app.route("/persons/:id").get((req: express.Request, res: express.Response) => {
+            this.loanUseCase.findPersonById(Number(req.params.id)).then((response) => {
+                res.status(200).send({
+                    response
+                });
+            }).catch(reason => {
+                res.status(400).send({
+                    "error": reason
+                });
+            });
+        });
+
+        app.route("/persons/:document").get((req: express.Request, res: express.Response) => {
+            this.loanUseCase.findPersonByDocument(req.params.document).then((response) => {
+                res.status(200).send({
+                    response
+                });
+            }).catch(reason => {
+                res.status(400).send({
+                    "error": reason
+                });
+            });
+        });
+
+        app.route("/persons/:id").delete((req: express.Request, res: express.Response) => {
+            this.loanUseCase.deletePerson(Number(req.params.id)).then((response) => {
+                res.status(200).send({
+                    response
+                });
+            }).catch(reason => {
+                res.status(400).send({
+                    "error": reason
+                });
+            });
+        });
     }
 }
